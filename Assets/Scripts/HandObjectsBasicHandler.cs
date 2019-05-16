cc946baa616f6450dd7a06ba53e22ef2af4b99ec
David <dgarciamateo.22@gmail.com>
2019-05-16 18:28:55 +0200

    CHANGE hand objects handler

---
+++ Assets/Scripts/HandObjectsBasicHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HandObjectsBasicHandler : MonoBehaviour
+{
+    public string tagRequired;
+    public Material visibleTexture;
+    public Material invisibleTexture;
+    protected GameObject collisioningObject;
+    private bool visible = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == tagRequired && !visible)
+        {
+            changeVisibility();
+            collisioningObject = other.gameObject;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == tagRequired && visible)
+        {
+            changeVisibility();
+            collisioningObject = null;
+        }
+        
+    }
+
+    private void changeVisibility()
+    {
+        visible = !visible;
+        GetComponent<Renderer>().sharedMaterial = visible ? visibleTexture : invisibleTexture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoWhatever();
+    }
+
+    public abstract void DoWhatever();
+
+}
--- Assets/Scripts/RightRayController.cs
+++ Assets/Scripts/RightRayController.cs
@@ -4,56 +4,23 @@ using UnityEngine;
 using Valve.VR;
 using UnityEngine.SceneManagement;
 
-public class RightRayController : MonoBehaviour
+public class RightRayController : HandObjectsBasicHandler
 {
-    private string menuItemTag = "INTERACTABLE";
-    public Material visibleTexture;
-    public Material invisibleTexture;
-    private bool visible = false;
+
     public GameObject teleportingObject;
-    public GameObject table;
-    public GameObject rightHand;
-    private GameObject collisioningObject;
 
-    private void OnTriggerEnter(Collider other)
+    public override void DoWhatever()
     {
-        if (other.gameObject.tag == menuItemTag && !visible)
+        if (CheckConditionToDo())
         {
-            changeVisibility();
-        }
-        Debug.Log(other.gameObject.tag);
-        collisioningObject = other.gameObject;
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == menuItemTag && visible)
-        {
-            changeVisibility();
-        }
-        collisioningObject = null;
-    }
-
-    private void changeVisibility()
-    {
-        visible = !visible;
-        GetComponent<Renderer>().sharedMaterial = visible ? visibleTexture : invisibleTexture;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (SteamVR_Actions.default_GrabPinch.state) {
-            if (visible)
-            {
-                Destroy(teleportingObject);
-                SceneManager.LoadScene("Instituto");
-            }
-
-            if (collisioningObject != null && collisioningObject.tag == "createTable")
-            {                
-                Instantiate(table, new Vector3(rightHand.transform.position.x, rightHand.transform.position.y, rightHand.transform.position.z), Quaternion.identity);
-            }
+            Destroy(teleportingObject);
+            SceneManager.LoadScene(collisioningObject.GetComponent<ItemToNavigate>().goTo);
         }
     }
+
+    public bool CheckConditionToDo()
+    {
+        return SteamVR_Actions.default_GrabPinch.state;
+    }
+
 }
